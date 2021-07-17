d0dc05fb1466f437a963d376a6078ce2eaf9a80e
zzzzzzzs <1443539042@qq.com>
2021-07-17 22:01:49 +0800

    Update JOLSample_01_Basic.java

--- java/src/main/java/com/me/jol/JOLSample_01_Basic.java
+++ java/src/main/java/com/me/jol/JOLSample_01_Basic.java
@@ -18,7 +18,7 @@ public class JOLSample_01_Basic {
     public static void main(String[] args) {
         byte a = '1';
         out.println(VM.current().details());
-        out.println(FieldLayout);
+//        out.println(FieldLayout);
         out.println(ClassLayout.parseClass(A.class).toPrintable());
     }
 
