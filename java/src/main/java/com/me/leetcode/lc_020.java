f68127f0b109d76bfa80ea72113eafba53c10cde
zzzzzzzs <1443539042@qq.com>
2021-10-07 17:06:11 +0800

    020 有效的括号

---
+++ java/src/main/java/com/me/leetcode/lc_020.java
@@ -0,0 +1,31 @@
+package com.me.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @author zs
+ * @date 2021/10/7.
+ * https://github.com/azl397985856/leetcode/blob/master/problems/20.valid-parentheses.md
+ */
+public class lc_020 {
+    public static boolean isValid(String s) {
+        if (s.length() < 2) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (c == '{') stack.push('}');
+            else if (c == '(') stack.push(')');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || c != stack.pop()) return false;
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "(){}}{";
+        boolean valid = isValid(s);
+        System.out.println(valid);
+    }
+}
