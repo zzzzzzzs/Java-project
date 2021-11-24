4cd4737194d9ff567b875f717e10243897e8f7e9
zzzzzzzs <1443539042@qq.com>
2021-11-24 08:40:13 +0800

    测试菜单程序

--- java/src/main/java/com/me/menu/MenuTest.java
+++ java/src/main/java/com/me/menu/MenuTest.java
@@ -5,6 +5,7 @@ import cn.hutool.core.lang.tree.Tree;
 import cn.hutool.core.lang.tree.TreeNode;
 import cn.hutool.core.lang.tree.TreeNodeConfig;
 import cn.hutool.core.lang.tree.TreeUtil;
+import com.alibaba.fastjson.JSON;
 
 import java.util.List;
 
@@ -19,21 +20,24 @@ public class MenuTest {
         List<TreeNode<String>> nodeList = CollUtil.newArrayList();
 
         nodeList.add(new TreeNode<>("1", "0", "系统管理", 5));
-        nodeList.add(new TreeNode<>("11", "1", "用户管理", 222222));
-        nodeList.add(new TreeNode<>("111", "11", "用户添加", 0));
-        nodeList.add(new TreeNode<>("2", "0", "店铺管理", 1));
-        nodeList.add(new TreeNode<>("21", "2", "商品管理", 44));
         nodeList.add(new TreeNode<>("221", "2", "商品管理2", 2));
+        nodeList.add(new TreeNode<>("2", "0", "店铺管理", 1));
+        nodeList.add(new TreeNode<>("11", "1", "用户管理", 222222));
+        nodeList.add(new TreeNode<>("21", "2", "商品管理", 44));
+        nodeList.add(new TreeNode<>("111", "11", "用户添加", 0));
 
-//配置
+        //配置
         TreeNodeConfig treeNodeConfig = new TreeNodeConfig();
-// 自定义属性名 都要默认值的
+        // 自定义属性名 都要默认值的
         treeNodeConfig.setWeightKey("order");
+        treeNodeConfig.setParentIdKey("pid");
         treeNodeConfig.setIdKey("rid");
-// 最大递归深度
-        treeNodeConfig.setDeep(3);
+        treeNodeConfig.setNameKey("menu_name");
+        treeNodeConfig.setWeightKey("sort");
+        // 最大递归深度
+//        treeNodeConfig.setDeep(3);
 
-//转换器
+        //转换器，可以属性的名字改掉
         List<Tree<String>> treeNodes = TreeUtil.build(nodeList, "0", treeNodeConfig,
                 (treeNode, tree) -> {
                     tree.setId(treeNode.getId());
@@ -45,7 +49,12 @@ public class MenuTest {
                     tree.putExtra("other", new Object());
                 });
 
-        treeNodes.forEach(e-> System.out.println(e.getName()));
+//        treeNodes.forEach(e -> System.out.println(e.getName()));
+        String json = JSON.toJSONString(treeNodes);
+        System.out.println(json);
+
+//        List<Tree<String>> build = TreeUtil.build(nodeList, "0");
+//        System.out.println(build);
 
 
     }
---
+++ java/src/main/test/NULL.java
@@ -0,0 +1,16 @@
+import java.util.function.Supplier;
+
+/**
+ * @author zs
+ *@date 2021/9/26.
+
+ */
+public class NULL {
+    public static void haha() {
+        System.out.println("haha");
+    }
+
+    public static void main(String[] args) {
+        ((NULL)null).haha();
+    }
+}
