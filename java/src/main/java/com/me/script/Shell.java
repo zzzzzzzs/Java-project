d4336161b3481195f2ff309924dfdf57608bcf9d
zzzzzzzs <1443539042@qq.com>
2022-04-19 21:08:14 +0800

    update

---
+++ java/src/main/java/com/me/script/Shell.java
@@ -0,0 +1,24 @@
+package com.me.script;
+
+/**
+ * @author zs
+ * @date 2022/4/19
+ */
+public class Shell {
+  // 调用python脚本
+  public static void python(String[] args) {
+    try {
+      Process process = Runtime.getRuntime().exec(args);
+      process.waitFor();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  public static void main(String[] args) {
+    python(
+        new String[] {
+          "python",
+          "D:\\Data\\KnowledgeWarehouse\\code\\Java-project\\java\\src\\main\\python\\test.py"
+        });
+  }
+}
