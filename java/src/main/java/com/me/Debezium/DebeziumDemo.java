f8f4f1a75eded69dc5a39a955e94c01e91a895cc
赵硕 <1443539042@qq.com>
2022-08-06 12:10:19 +0800

    Update DebeziumDemo.java

--- java/src/main/java/com/me/Debezium/DebeziumDemo.java
+++ java/src/main/java/com/me/Debezium/DebeziumDemo.java
@@ -5,6 +5,7 @@ import io.debezium.engine.ChangeEvent;
 import io.debezium.engine.DebeziumEngine;
 import io.debezium.engine.format.Json;
 import io.debezium.relational.history.FileDatabaseHistory;
+import org.apache.kafka.connect.storage.FileOffsetBackingStore;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
@@ -56,30 +57,31 @@ public class DebeziumDemo {
     // 在maven处引入其他数据库的连接器，例如debezium-connector-postgres，再修改此处的connector.class，即可使用其他数据库的CDC
     props.setProperty("connector.class", MySqlConnector.class.getCanonicalName());
     props.setProperty("database.server.name", "my_server_01"); // 可以任意修改
-    props.setProperty("database.hostname", "localhost"); // IP
+    props.setProperty(
+        "database.hostname", "pro-spark-shop-master-public.mysql.rds.aliyuncs.com"); // IP
     props.setProperty("database.port", String.valueOf(3306)); // 端口
-    props.setProperty("database.user", "cdc_user"); // 用户
-    props.setProperty("database.password", "cdc_password"); // 密码
+    props.setProperty("database.user", "dev_zhaoshuo"); // 用户
+    props.setProperty("database.password", "$m!hQ!X&j%nZa#KnB"); // 密码
     props.setProperty("database.serverTimezone", "UTC"); // 时区
     // 下面两个是数据库和表，注意只能选择一种:
     // 1. 使用database.whitelist，只设置数据库（会通知全库的CDC信息）
     // 2. 使用table.whitelist，设置库名和表名（会通知单个库的单个表的CDC信息）
     //        props.setProperty("database.whitelist", "db_inventory_cdc");
-    props.setProperty("table.whitelist", "db_inventory_cdc.tb_products_cdc"); // 库.表名
+    //    props.setProperty("table.whitelist", "db_inventory_cdc.tb_products_cdc"); // 库.表名
 
-    props.setProperty("name", "engine");
+//    props.setProperty("name", "engine");
     // props.setProperty("offset.storage", FileOffsetBackingStore.class.getCanonicalName());
     // props.setProperty("offset.storage.file.filename", "./storage/offset.dat");
     // props.setProperty("offset.flush.interval.ms", String.valueOf(60000L));
-    props.setProperty("key.converter.schemas.enable", "false");
-    props.setProperty("value.converter.schemas.enable", "false");
-    props.setProperty("include.schema.changes", "false");
-    props.setProperty("tombstones.on.delete", "false");
-    props.setProperty("database.history", FileDatabaseHistory.class.getCanonicalName());
-    props.setProperty("database.history.store.only.monitored.tables.ddl", "true");
-    props.setProperty("database.history.file.filename", "./storage/dbhistory.dat");
-    props.setProperty("database.history.instance.name", UUID.randomUUID().toString());
-    props.setProperty("database.history.skip.unparseable.ddl", "true");
+    //    props.setProperty("key.converter.schemas.enable", "false");
+    //    props.setProperty("value.converter.schemas.enable", "false");
+    //    props.setProperty("include.schema.changes", "false");
+    //    props.setProperty("tombstones.on.delete", "false");
+    //    props.setProperty("database.history", FileDatabaseHistory.class.getCanonicalName());
+    //    props.setProperty("database.history.store.only.monitored.tables.ddl", "true");
+    //    props.setProperty("database.history.file.filename", "./storage/dbhistory.dat");
+    //    props.setProperty("database.history.instance.name", UUID.randomUUID().toString());
+    //    props.setProperty("database.history.skip.unparseable.ddl", "true");
 
     return props;
   }
