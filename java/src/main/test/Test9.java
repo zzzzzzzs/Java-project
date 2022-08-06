1512b06f53f475d829cb54a3f32a96b1479234fa
赵硕 <1443539042@qq.com>
2022-08-06 11:47:01 +0800

    Update Test9.java

--- java/src/main/test/Test9.java
+++ java/src/main/test/Test9.java
@@ -1,11 +1,10 @@
-// 一致性哈希
-
-
-import com.google.common.graph.GraphBuilder;
-
+import org.json.JSONObject;
 public class Test9 {
   public static void main(String[] args) {
-      GraphBuilder<Object> directed = GraphBuilder.directed();
-      directed.allowsSelfLoops(false);
+    String jsonStr = "{\"id\":12,\"timestamp\":\"2020-09-15T10:00:00.000+08:00\",\"temperature\":\"25.5\"}";
+    JSONObject jsonObject = new JSONObject(jsonStr);
+    int id = jsonObject.getInt("id");
+    String timestamp = jsonObject.getString("timestamp");
+    System.out.println(id);
   }
 }
